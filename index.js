8ed3b34a78afb3423feac4962645d904573dbdcc
Yair Nevet <yair.nevet@omni-verse.ae>
2023-03-18 14:10:49 +0400

    update logic

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
+const { connected } = require("process");
+
+async function checkFileExistence(path) {
+  return fs.promises
+    .access(path, fs.constants.F_OK)
+    .then(() => {
+      core.info(`${path} exists`);
+      return true;
+    })
+    .catch(() => {
+      core.setFailed(`${path} does not exist`);
+      return false;
+    });
+}
 
 (async () => {
   try {
-    core.notice("Check File Action called!!!");
+    checkFileExistence("README.md");
+    checkFileExistence("LICENSE");
   } catch (error) {
     core.setFailed(error.message);
   }
