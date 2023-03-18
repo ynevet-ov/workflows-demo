7420af702c5a173c36f011367b4fa0762e4eb879
Yair Nevet <yair.nevet@omni-verse.ae>
2023-03-18 14:16:54 +0400

    check first line is title

--- index.js
+++ index.js
@@ -16,10 +16,56 @@ async function checkFileExistence(path) {
     });
 }
 
+// create a function that checks if the file starts with a markdown header
+async function checkFileStartsWithHeader(filePath) {
+  return fs.promises.readFile(filePath, "utf8").then((fileContent) => {
+    // remove all empty lines ad the beginning of the file
+    fileContent = fileContent.replace(/^\s*\n/gm, "");
+
+    if (fileContent.startsWith("#")) {
+      core.info(`File ${filePath} starts with a header`);
+      return true;
+    } else {
+      core.setFailed(`File ${filePath} does not start with a header`);
+      return false;
+    }
+  });
+}
+
 (async () => {
   try {
     checkFileExistence("README.md");
     checkFileExistence("LICENSE");
+    if (!(await checkFileStartsWithHeader("README.md"))) {
+      // get token for octokit
+      const token = core.getInput("repo-token");
+      const octokit = new github.getOctokit(token);
+
+      // call octokit to create a check with annotation and details
+      const check = await octokit.rest.checks.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: "Readme Validator",
+        head_sha: github.context.sha,
+        status: "completed",
+        conclusion: "failure",
+        output: {
+          title: "README.md must start with a title",
+          summary: "Please use markdown syntax to create a title",
+          annotations: [
+            {
+              path: "README.md",
+              start_line: 1,
+              end_line: 1,
+              annotation_level: "failure",
+              message: "README.md must start with a header",
+              start_column: 1,
+              end_column: 1,
+            },
+          ],
+        },
+      });
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
